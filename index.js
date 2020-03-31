d4ffda9dea5c4e2b849305090aa7986f4a57d0dc
FUAT ÇAKIR <37143344+fuatcakir@users.noreply.github.com>
2020-03-31 11:33:03 +0300

    add Access-Control-Allow-Origin to header

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ let app = express();
 let apiRoutes = require('./api-routes');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use(bodyParser.json());
 
 const uri = "mongodb+srv://fuatcakir:MB_41hf414@clusterfc-3xfqs.mongodb.net/vacationplans?retryWrites=true&w=majority";
